2068c840cd76762fb43f3b77e7253791287854d7
LorynnPerell <lcperell@students.unwsp.edu>
2025-09-05 22:07:40 -0500

    Assignment 2 redo

--- program_1.py
+++ program_1.py
@@ -1,15 +1,10 @@
 def personal_information():
-    print('Lorynn Perell')
-    Adress = "City Hall, City Hall Park, New York"
-    print(Adress)
-    zipcode = "10007"
-    print(zipcode)
-    Phone_number = "651-763-5500"
-    print(Phone_number)
-    College_major = "Computer science and criminology"
-    print(College_major)
-    
-    
+  print("Name: Lorynn Perell")
+  print("Address: 1234 Main St, New York, NY 10001")
+  print("Zipcode: 10007")
+  print("Phone number: 651-763-5500")
+  print("College major: Computer science and criminology")
+
 
 # Line which calls the above function.
 personal_information()
