dbe6e62b1e8f22a134d8ea50085bc5ed816b5d80
LorynnPerell <lcperell@students.unwsp.edu>
2025-09-10 01:09:56 +0000

    Assignmetn redo again

--- program_4.py
+++ program_4.py
@@ -9,7 +9,6 @@ def temp_conversion(celsius):
     # Calculate the Fahrenheit equivalent.
     fahrenheit = 0.0
     ######################
-    celsius = float(input("Enter temperature in Celsius: "))
     fahrenheit = (9/5) * celsius + 32
     print(celsius, "degrees Celsius is", fahrenheit, "degrees Fahrenheit. ")
     ######################    
